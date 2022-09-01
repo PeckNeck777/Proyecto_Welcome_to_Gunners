53e04397ae2dae9e0c3acc46b0847f8e88a27f00
PeckNeck777 <110553489+PeckNeck777@users.noreply.github.com>
2022-09-01 10:39:22 -0600

    Mecanicas de las camaras
    Canvas de las camaras ya funcional

--- Assets/Escenarios/Pizzeria completa/Scripts/Camaras.cs
+++ Assets/Escenarios/Pizzeria completa/Scripts/Camaras.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Camaras : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject canvasoficina;
+    [SerializeField] private GameObject canvascamaras;
+
+    [SerializeField] private Camera[] arraycamaras;
+    [SerializeField] private Camera camaraactual;
+
+    public void activarcamaras()
     {
-        
+        canvasoficina.SetActive(false);
+        canvascamaras.SetActive(true);
+        arraycamaras[0].enabled = false;
+        camaraactual = arraycamaras[1];
+        camaraactual.enabled = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void cambicamaras(int numerocamara)
     {
-        
+        camaraactual.enabled = false;
+        camaraactual = arraycamaras[numerocamara];
+        camaraactual.enabled = true;
+
+    }
+    public void regreso()
+    {
+        canvasoficina.SetActive(true);
+        canvascamaras.SetActive(false);
+
+        cambicamaras(0);
     }
 }
+
